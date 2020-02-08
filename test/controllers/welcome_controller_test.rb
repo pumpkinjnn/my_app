6e04141e6755afd0804a2427d6ae2b45db419e0f
Nannan Ji <jinannan@grinnell.edu>
2020-02-08 10:38:46 -0600

    adding tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,6 +4,21 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get homepage" do
     get welcome_homepage_url
     assert_response :success
+    assert_select "title", "Spectral by HTML5 UP"
   end
 
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+  test "should get generic" do
+    get welcome_generic_url
+    assert_response :success
+  end
+
+  test "should get elements" do
+    get welcome_elements_url
+    assert_response :success
+  end
 end
