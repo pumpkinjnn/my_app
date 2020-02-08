5be6d1f4c866c40bd7b832804c09778706c0d46b
Nannan Ji <jinannan@grinnell.edu>
2020-02-08 10:11:18 -0600

    update using html/css template

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def homepage
+  end
+
+  def index
+  end
+
+  def generic
+  end
+
+  def elements
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'welcome/homepage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#homepage'
+
+  get 'welcome/index'
+
+  get 'welcome/generic'
+
+  get 'welcome/elements'
 end
