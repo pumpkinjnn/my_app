facb7f09faf571529598654ec0584571dd1494a5
Nannan Ji <jinannan@grinnell.edu>
2020-02-17 00:01:28 -0600

    hm4: part 2, update gallery w/ images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,4 +12,5 @@ class WelcomeController < ApplicationController
 
   def elements
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'welcome/generic'
 
   get 'welcome/elements'
+
 end
