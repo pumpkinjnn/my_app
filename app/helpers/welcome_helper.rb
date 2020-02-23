550f231768f20d65f61490964a82d7d788f8b6c4
Nannan Ji <jinannan@grinnell.edu>
2020-02-22 22:46:42 -0600

    HW6: add test for random image, update tests

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,7 @@
 module WelcomeHelper
+  def get_random_image
+    image_path_prefix = "app/assets/images/"
+    image_files = Dir.glob("#{image_path_prefix}photography/*")
+    image_files.sample.split(image_path_prefix)[1]
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
+
+  include WelcomeHelper
+
+  def setup
+    @base_title = " | Nannan"
+    @photogtaphy = "app/assets/images/"
+  end
+
   test "should get homepage" do
     get welcome_homepage_url
     assert_response :success
-    assert_select "title", "Spectral by HTML5 UP"
+    assert_select "title", "Home#{@base_title}"
   end
 
   test "should get index" do
@@ -12,13 +20,22 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get generic" do
+  test "should get photography" do
     get welcome_generic_url
     assert_response :success
+    assert_select "title", "Photography#{@base_title}"
   end
 
   test "should get elements" do
     get welcome_elements_url
     assert_response :success
+    assert_select "title", "Elements#{@base_title}"
   end
+
+  test "get_random_image gets not nil image" do
+    random_image_path = get_random_image
+    #put random_image_path
+    assert File.exist?("#{@photogtaphy}"+ random_image_path), random_image_path
+  end
+
 end
